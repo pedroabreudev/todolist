51661e4df4b9ff0ffea74b1cec4bf2b2656a90b5
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2023-10-09 22:45:49 -0400

    first commit

---
+++ src/main/java/br/com/pedroabreudev/todolist/user/UserController.java
@@ -0,0 +1,23 @@
+package br.com.pedroabreudev.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /*
+     * Body
+     */
+    
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+        System.out.println(userModel.username);
+        System.out.println(userModel.password);
+    }
+
+}
