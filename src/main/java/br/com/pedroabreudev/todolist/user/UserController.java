1f706940f32cff38004b8cda5f1981449198a8bd
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2023-10-10 17:49:36 -0400

    Aula 02 - Integração com Banco de Dados

--- src/main/java/br/com/pedroabreudev/todolist/user/UserController.java
+++ src/main/java/br/com/pedroabreudev/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.pedroabreudev.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     /*
      * Body
      */
-    
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
-        System.out.println(userModel.username);
-        System.out.println(userModel.password);
-    }
 
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {           
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
 }
